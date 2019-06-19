3a4b8a2da0732da871d12d805e23c9083f7ad67a
Auro@Python <36454560+Aurobinda2@users.noreply.github.com>
2019-06-19 09:51:06 +0530

    Update test_Request.py

--- test_Request.py
+++ test_Request.py
@@ -6,8 +6,8 @@ data=requests.get(baseUrl+uri,params)
 data_dict=data.json()
 #print(data_dict)
 for item in data_dict['data']:
-    print('id:'item['id'])
-    print('Name:' item['first_name']+' '+item['last_name'])
+    print('id:',item['id'])
+    print('Name:', item['first_name']+' '+item['last_name'])
     print(' ')
     
 

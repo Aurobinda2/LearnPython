1d229484fc3b841f401a501b4ff5283c8cacfbf2
Auro@Python <36454560+Aurobinda2@users.noreply.github.com>
2019-06-19 09:43:08 +0530

    Update test_Request.py

--- test_Request.py
+++ test_Request.py
@@ -4,4 +4,10 @@ uri='/api/users'
 params={'page':2}
 data=requests.get(baseUrl+uri,params)
 data_dict=data.json()
-print(data_dict)
+#print(data_dict)
+for item in data_dict['data']:
+    print('id:'item['id'])
+    print('Name:' item['first_name']+' '+item['last_name'])
+    print(' ')
+
+    
